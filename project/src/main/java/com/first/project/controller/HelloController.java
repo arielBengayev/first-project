939f49349318d993afcca72fc138544706a77b9d
arielBengayev <arielazaz@gmail.com>
2025-11-10 15:37:23 +0200

    remove swagger

--- project/src/main/java/com/first/project/controller/HelloController.java
+++ project/src/main/java/com/first/project/controller/HelloController.java
@@ -8,17 +8,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/hello")
 public class HelloController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public ResponseEntity<?> hello(){
         return new ResponseEntity<>("Hello World!", HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/bye", method = RequestMethod.GET)
-    public ResponseEntity<?> bye(){
-        return new ResponseEntity<>("bye World!", HttpStatus.OK);
     }
 
 }
