9c9fac6046322d9977c936cdbe623494fde700f4
arielBengayev <arielazaz@gmail.com>
2025-11-10 12:38:40 +0200

    with controller

---
+++ project/src/main/java/com/first/project/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.first.project.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public ResponseEntity<?> hello(){
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/bye", method = RequestMethod.GET)
+    public ResponseEntity<?> bye(){
+        return new ResponseEntity<>("bye World!", HttpStatus.OK);
+    }
+
+}
