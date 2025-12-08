63f3768ba24c3776d5d49e0c962dcc11e129e665
arielBengayev <arielazaz@gmail.com>
2025-12-08 16:59:05 +0200

    with data

--- project/src/main/java/com/first/project/model/Student.java
+++ project/src/main/java/com/first/project/model/Student.java
@@ -11,28 +11,25 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
-@Entity @Table(name="student2")
+@Entity @Table(name="student3")
 @Getter @Setter @AllArgsConstructor
 public class Student implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty
-    @Length(max = 30)
+    @NotEmpty @Length(min = 5, max = 30)
     private String fullName;
 
-    @Min(100)
-    @Max(800)
+    @Min(100) @Max(800)
     private Integer psycoScore;
 
-    @Min(30)
-    @Max(100)
+    @Min(30) @Max(100)
     private Double graduationScore;
 
-    @Length(max = 10)
+    @Length(min = 10, max = 10)
     private String phone;
 
     @Length(max = 500)
